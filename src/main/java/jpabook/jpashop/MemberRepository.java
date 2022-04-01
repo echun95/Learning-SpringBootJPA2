2a18980e9420efcc1f79f5662c1b5faf58d0d730
leeyoungchun <dldydcns123@gmail.com>
2022-04-01 22:04:17 +0900

    JPA, DB설정 및 테스트

---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    /**
+     * 유저 정보 저장
+     * @param member
+     * @return
+     */
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
+
+}
