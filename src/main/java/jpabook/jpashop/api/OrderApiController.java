da2b8996136d8c6ca802ebeae366abc122aff415
leeyoungchun <dldydcns123@gmail.com>
2022-04-16 15:56:51 +0900

    주문 조회 v4 : jpa에서 dto 직접 조회 - 컬렉션 조회 최적화

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -72,7 +72,10 @@ public class OrderApiController {
         return orderQueryRepository.findOrderQueryDtos();
     }
 
-
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5(){
+        return orderQueryRepository.finAllByDto_optimization();
+    }
 
 
 
